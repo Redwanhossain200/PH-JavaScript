508f16047be2b1c0fed8426a6cc12002dd3e5a12
Redwanhossain200 <mdredwanhossain2007@gmail.com>
2026-01-25 14:57:20 +0600

    learning array

--- js-array/array.js
+++ js-array/array.js
@@ -5,4 +5,21 @@
 
 // const numbers = 123343546;
 
-const numbers = [12 , 22 , 54 , 65 , 56];
+const numbers = [11, 22, 54, 65, 56, 22, 33, 45, 67, 87, 11, 74];
+// length 
+console.log(numbers.length);
+
+
+const marks = [10, 22, 54, 65, 56, 22, 33];
+// Index 
+console.log(marks[5]);
+
+const fourth = marks[6];
+console.log(fourth)
+
+// set or update elment by index
+let abc = 88;
+abc = 99;
+
+numbers[1] = 999;
+console.log(numbers)
