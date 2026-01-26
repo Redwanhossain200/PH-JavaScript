4982acca89faccfa5b790df4d819f6359b49f554
Redwanhossain200 <mdredwanhossain2007@gmail.com>
2026-01-26 21:39:39 +0600

    learning for loop

---
+++ js-loop/for-of.js
@@ -0,0 +1,16 @@
+// let numbers = [12, 22, 34, 56, 94, 34, 60, 10];
+
+// console.log(numbers);
+
+// for (const num of numbers) {
+//   console.log(num);
+// }
+
+
+
+
+const fruits = ['Orange', 'Banana', 'Watermelon', 'Apple', 'Jackfruit'];
+
+for (const fruit of fruits){
+  console.log(fruit, 'I want to eat');
+}
