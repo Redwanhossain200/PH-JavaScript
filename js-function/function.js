58618856885454bb2c42ee7769c0ea9eb0a49211
Redwanhossain200 <mdredwanhossain2007@gmail.com>
2026-01-31 21:26:42 +0600

    learning js function

---
+++ js-function/function.js
@@ -0,0 +1,44 @@
+const price = 45;
+
+// function declare   
+function fanOffKor() {
+  console.log('Bosa theke the dara');
+  console.log('walk towards the switch');
+  console.log('click the switch off of the fan');
+}
+
+// call the function
+fanOffKor();
+
+// function name(){
+
+// }
+function brushTeeth() {
+  console.log('pick up the brush');
+  console.log('add paste');
+  console.log('2 min brush kore fena felo');
+}
+
+// call the function
+console.log('-------------')
+console.log('-------------')
+console.log('-------------')
+console.log('learning function')
+brushTeeth();
+
+
+
+function machertorkari(mach, shobji, mosla) {
+  // rannar instruction (console.log)
+  console.log('tel gorom koro');
+  console.log('mosola foran');
+  console.log('add some vegetables');
+  console.log(mach + 'din');
+
+  // rannar sesh (return value)
+  return "delicious food is ready"
+}
+
+// function call 
+const result = machertorkari("hilsha", "potato", "holud");
+  console.log(result); //result print
---
+++ js-function/parameter.js
@@ -0,0 +1,44 @@
+function vaatkhao() {
+  console.log('hand wash kore eso')
+  console.log('boso')
+  console.log('plate e khabar dabar nao')
+  console.log('khawa suru kor')
+}
+
+vaatkhao();
+
+
+// 3 --> 3*3 = 9
+// 4 --> 4*4 = 16
+// 5 --> 5*5 = 25
+// 11 --> 11*11 = 121
+
+
+function square(number) {
+  console.log('value of the number parameter', number);
+  const square = number * number;
+  console.log('square of the given number is', square);
+}
+
+square(4);
+console.log('-------------');
+square(12);
+square(5);
+square(405);
+
+
+
+function add(num1,num2) {
+  const sum = num1 + num2;
+  console.log(sum);
+}
+add(5,71);
+add(56,31);
+
+
+
+function addAll(a , b , c , d , e) {
+  const total = a + b + c + d + e;
+  console.log(total);
+}
+addAll(23, 65, 92, 74, 25);
