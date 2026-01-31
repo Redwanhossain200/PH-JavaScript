78ed51ffc219ebefa521d7c0948f75486063fcd3
Redwanhossain200 <mdredwanhossain2007@gmail.com>
2026-01-31 13:32:04 +0600

    fixed code

--- js-conditional/variable.js
+++ js-conditional/variable.js
@@ -13,19 +13,20 @@ var passed = true;
 
 // Naming Convention
 
-// 1.Single word
-// 2.no quote
-// 3.no gap or dash
-// 4.can not start with number
-// 5.can not be a caracter
+// 1. Single word
+// 2. no quote
+// 3. no gap or dash
+// 4. can not start with number
+// 5. can not be a keyword
+// 6. prefer camelCase
 
 
 var lastExamResult = 89;
 
 // Operation
 
-// 1.+ , _ , * , / , %
-// 2.+= , -= , *= , /=
-// 3.isNan?
-// 4.null 
-// 5.Number 
+// 1. + , _ , * , / , %
+// 2. += , -= , *= , /=
+// 3. isNan?
+// 4. null 
+// 5. Number 
