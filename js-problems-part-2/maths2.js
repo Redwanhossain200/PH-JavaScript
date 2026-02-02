4b7fee88bb2aa06eb8c591386ee9ea0627070b8a
Redwanhossain200 <mdredwanhossain2007@gmail.com>
2026-02-02 22:41:45 +0600

    add file

---
+++ js-problems-part-2/maths2.js
@@ -0,0 +1,11 @@
+const jim = 56;
+const tim = 89;
+const kim = 68;
+
+if (jim > tim && jim > kim) {
+  console.log('Jim is the ultimate boss');
+} else if(tim > jim && tim > kim) {
+  console.log('Tim is the ultimate boss');
+} else {
+  console.log('Kim is the ultimate boss');
+}
