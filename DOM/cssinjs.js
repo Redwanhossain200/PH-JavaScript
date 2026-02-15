a55fdf2f109864eb7da3add8dab044324ef9384c
Redwanhossain200 <mdredwanhossain2007@gmail.com>
2026-02-15 13:43:04 +0600

    DOM learning

--- DOM/cssinjs.js
+++ DOM/cssinjs.js
@@ -1,6 +1,16 @@
 // const sections = document.getElementsByTagName('section');
 const sections = document.querySelectorAll('section');
 // console.log(sections);
+// for (const section of sections) {
+//   console.log(section);
+//   section.style.backgroundColor = 'teal';
+//   section.style.color = 'white';
+//   section.style.marginBottom = '10px';
+//   section.style.padding = '30px';
+//   section.style.borderRadius = '15px';
+//   section.style.border = '2px solid purple';
+// }
+
 for (const section of sections) {
-  console.log(section);
+  section.classList.add('section-card');
 }
