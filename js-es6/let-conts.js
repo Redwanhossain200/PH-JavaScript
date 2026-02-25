f80ac8e833ec73433ceaab89f878cb5e83a6d838
Redwanhossain200 <mdredwanhossain2007@gmail.com>
2026-02-26 00:40:49 +0600

    learning ES6

---
+++ js-es6/let-conts.js
@@ -0,0 +1,14 @@
+// const name = "Bangladesh";
+// // name = "Singapore";
+// const countryName = "BD " + name;
+// console.log(countryName);
+
+// const price = 120;
+// price = 150;
+// console.log(price);
+
+// var name2 = 'Bangladesh';
+
+const dress = ['shirt', 'pant', 'shoe'];
+dress.push('Belt');
+console.log(dress);
