79b70af67aa7978a5fe8ee1f3e54d830eabb90a3
Redwanhossain200 <mdredwanhossain2007@gmail.com>
2026-02-27 00:51:33 +0600

    chain

--- js-es6/chain.js
+++ js-es6/chain.js
@@ -3,7 +3,15 @@ const employee = {
   1: 'desk one',
   'home-address': '123 Main St',
   salary: 50000,
+  family: {
+    father: 'Richard Doe',
+    mother: {
+      name: 'Jane Doe',
+      age: 55
+    }
+  },
   position: 'Software Engineer'
 };
+
 const value = employee.name;
-console.log();
+console.log(employee.family?.mother?.age);
