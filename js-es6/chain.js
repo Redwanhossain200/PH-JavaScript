4f011e46bde23789720f56f29f37f5af490dfa42
Redwanhossain200 <mdredwanhossain2007@gmail.com>
2026-02-27 00:29:23 +0600

    memorizing some ES6 functions

---
+++ js-es6/chain.js
@@ -0,0 +1,9 @@
+const employee = {
+  name: 'John Doe',
+  1: 'desk one',
+  'home-address': '123 Main St',
+  salary: 50000,
+  position: 'Software Engineer'
+};
+const value = employee.name;
+console.log();
--- js-es6/freeze.js
+++ js-es6/freeze.js
@@ -1,3 +1,8 @@
 const king = { name: 'Mufasa', age: 25, kingdom: 'Pride lands' };
+// Object.freeze(king);
+Object.seal(king);
 delete king.age;
+delete king.kingdom;
+king.queen = 'Sarabi';
+king.name = 'King Simba';
 console.log(king);
